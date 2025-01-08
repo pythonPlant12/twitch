785199596c3a0bd61c034cd8af6bacb2a6ab8cd4
pythonPlant12 <pythonplant12@gmail.com>
2025-01-09 00:39:40 +0100

    - Added metadata field to express-session.d.ts SessionData - Added new dependencies for geolocation by ip and device detection:   - device-detector-js   - geopi-lite   - i18n-iso-countries - Added new interfaces in session-metadata.types.ts

---
+++ src/shared/types/session-metadata.types.ts
@@ -0,0 +1,24 @@
+export interface LocationInfo {
+	country: string
+	city: string
+	latitude: number
+	longitude: number
+}
+
+export interface DeviceInfo {
+	browser: string
+	os: string
+	type: string
+}
+
+/*
+Dependencies used for getting session metadata:
+  - geoip-lite - for country code of ip
+  - device-detector-js - for device detection
+  - i18n-iso-countries - for country name by country code
+*/
+export interface SessionMetadata {
+	location: LocationInfo
+	device: DeviceInfo
+	ip: number
+}
